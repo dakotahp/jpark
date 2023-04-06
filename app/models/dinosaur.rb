255ae018ea58bce9235f611b5df4c5a424037006
DP <git@dakotahpena.dev>
2023-04-06 15:37:38 -0700

    Fixed string literal syntax error

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,17 +1,19 @@
 class Dinosaur < ApplicationRecord
   CARNIVORES = [
-    "Tyrannosaurus"
+    "Tyrannosaurus",
     "Velociraptor",
     "Spinosaurus",
     "Megalosaurus"
   ]
 
   HERBIVORES = [
-    "Brachiosaurus"
-    "Stegosaurus"
+    "Brachiosaurus",
+    "Stegosaurus",
     "Ankylosaurus",
     "Triceratops"
   ]
 
   validates :name, presence: true
+  validates :species, inclusion: { in: (CARNIVORES+HERBIVORES),
+    message: "%{value} is not a valid species" }
 end
